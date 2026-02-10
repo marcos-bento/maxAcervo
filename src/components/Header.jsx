9aa9f5309fde3cc2220de5dd9a750c650f7aa5db
Marcos Bento <marcos.x.marcos@hotmail.com>
2026-02-09 23:38:46 -0300

    Update 09-02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Database } from 'lucide-react'
 
-function Header({ projetos }) {
+function Header({ totalProjetos }) {
   return (
       <header className="portfolio-header">
         <div className="page-container header-content">
@@ -15,7 +15,7 @@ function Header({ projetos }) {
             </div>
           </div>
           <div className="header-counter">
-            <span className="counter-number">{projetos.length}</span>
+            <span className="counter-number">{totalProjetos}</span>
             <span>projetos</span>
           </div>
         </div>
